db2f787cc8d24b1208e8a8775cf0f19a3fae8510
anteqkois <anteqkois.dev@gmail.com>
2022-12-17 00:40:32 +0100

    feat(auction): add auction api

--- pages/api/auction.ts
+++ pages/api/auction.ts
@@ -7,60 +7,69 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    console.log(req.body);
+  switch (req.method) {
+    case 'POST':
+      try {
+        console.log(req.body);
 
-    //CREATE IMAGE
-    const {
-      file,
-      fileName,
-      userAddress,
-      crashed,
-      CurrentPrice,
-      enginePower,
-      fuelType,
-      mark,
-      mileage,
-      model,
-      VIN,
-      year,
-    } = req.body;
+        //CREATE IMAGE
+        const {
+          file,
+          fileName,
+          userAddress,
+          crashed,
+          CurrentPrice,
+          enginePower,
+          fuelType,
+          mark,
+          mileage,
+          model,
+          VIN,
+          year,
+        } = req.body;
 
-    const uploadResponse = await cloudinary.uploader.upload(file, {
-      upload_preset: 'code-night-3',
-      folder: 'code-night-3',
-    });
+        const uploadResponse = await cloudinary.uploader.upload(file, {
+          upload_preset: 'code-night-3',
+          folder: 'code-night-3',
+        });
 
-    const auction = prisma.auction.create({
-      data: {
-        crashed,
-        CurrentPrice,
-        enginePower,
-        fuelType,
-        mark,
-        mileage,
-        model,
-        VIN,
-        year,
-        user: { connect: { address: userAddress } },
-        image: {
-          create: {
-            affixUrl: uploadResponse.public_id,
-            fileName,
-            id: uploadResponse.public_id,
-            url: uploadResponse.secure_url,
-            user: { connect: {} },
+        const auction = prisma.auction.create({
+          data: {
+            crashed,
+            CurrentPrice,
+            enginePower,
+            fuelType,
+            mark,
+            mileage,
+            model,
+            VIN,
+            year,
+            user: { connect: { address: userAddress } },
+            image: {
+              create: {
+                affixUrl: uploadResponse.public_id,
+                fileName,
+                id: uploadResponse.public_id,
+                url: uploadResponse.secure_url,
+                user: { connect: {} },
+              },
+            },
           },
-        },
-      },
-      include: { bids: true, image: true, user: true },
-    });
+          include: { bids: true, image: true, user: true },
+        });
 
-    // TODO create Bid and assign to auction PRISMA
+        // TODO create Bid and assign to auction PRISMA
 
-    res.status(200).json({ auction });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ err: 'Something went wrong' });
+        res.status(200).json({ auction });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: 'Something went wrong' });
+      }
+      break;
+
+    default:
+      const auctions = prisma.auction.findMany();
+      res.status(200).json({ auctions });
+      break;
   }
 }
