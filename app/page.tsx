8faa14c2267b92ad006226c3948dd735f1ae3a8d
eskeemos <l.michalik004@gmail.com>
2022-12-16 23:23:39 +0100

    carousel suu

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,22 @@ export default function Home() {
   return (
     <div>
       <main>
-        {/* <div className="min-h-screen bg-red-300"></div> */}
-        <div className="bg-black">
-          <SliderCarousel />
+        <div>
+          <div className="pt-5 h-96">
+            <div className='text-center py-4'>
+              <span className='text-center text-3xl px-10 pb-3 font-thin border-b-2 border-black'>Ostatnio wyszukiwane</span>
+            </div>
+            <SliderCarousel />
+          </div>
+          <div className='text-center bg-red-500'>
+            Tutaj trzeba zajebac jakis baner 
+          </div>
+          <div className="h-80 pt-5">
+            <div className='text-center py-4'>
+              <span className='text-center text-3xl px-10 pb-3 font-thin border-b-2 border-black'>Wybrane dla ciebie</span>
+            </div>
+            <SliderCarousel />
+          </div>
         </div>
       </main>
       <footer></footer>
--- src/components/utils/SliderCarousel.tsx
+++ src/components/utils/SliderCarousel.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import React from "react";
 import Slider from "react-slick";
+import CarouselArrow from './CarouselArrow';
 
 export default function SimpleSlider() {
   var settings = {
@@ -9,20 +10,22 @@ export default function SimpleSlider() {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    nextArrow: <CarouselArrow />,
+    prevArrow: <CarouselArrow />,
   };
   return (
-    <div className=" mx-32">
+    <div className="mx-32">
       <Slider {...settings} className="mx-2">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((x) =>
           <div className="my-4">
-            <div className="mx-auto bg-gray-500 w-72 h-60 text-center rounded">
-              <h3 className="text-sm font-medium py-3 rounded-t border-b-4 border-black">Mercedes-AMG S 63 E PERFORMANCE.</h3>
+            <div className="mx-auto w-72 h-60 text-center rounded-xl shadow-2xl">
+              <h3 className="text-sm font-medium py-3 rounded-t-xl border-b-2 border-black">Mercedes-AMG S 63 E PERFORMANCE.</h3>
               <Image className="block mx-auto pb-5" alt="car" width={208} height={1} src="https://assets.oneweb.mercedes-benz.com/iris/iris.jpg?COSY-EU-100-1713d0VXqNEFqtyO67PobzIr3eWsrrCsdRRzwQZg9pZbMw3SGtGyStsd2HdcUfp8qXGEubSJ0l3IJOB2NS1bApRTyI5uGoxQC30CQkzNBtum7jA2mhKViSF%25vq4tTyLRgLFYaxPrSrH1yBRn8wYTyoiZrMYM4FAK2Tg95Yn6PDCruSeWzQWtsd8htcUfircXGE4TXJ0lg6ZOB2PbnbApe79I5ul6xQC3vT6khQOZ9wJ1SeWvyVtsdPJ%25cUfxpBXGE0GYJ0lBHtOB2A85bAp5ilI5uCmZQC3zSTkzN7lbm7sDgubYwR9hDsxevqKj6hVNpLLxdYfqJVf%25XEd9B96N683eUHpi3v1LfIVf&imgt=P27&bkgnd=9&pov=BE040&uni=c&im=Crop,rect=(0,-25,1370,770),gravity=Center;Resize,width=350" />
-              <div className="border-t-4 border-black pt-3 flex justify-evenly">
+              <div className="border-t-2 border-black pt-3 flex justify-evenly">
                 <span>2016</span>
-                <span>92 000 km</span> 
-                <span>Benzyna</span> 
+                <span>92 000 km</span>
+                <span>Benzyna</span>
               </div>
             </div>
           </div>
