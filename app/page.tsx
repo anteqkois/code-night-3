ddacd530b728d6aae2709ec3e5c73a2f7c40d4ad
eskeemos <l.michalik004@gmail.com>
2022-12-16 22:31:15 +0100

    conflict resolve

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import Button from '../src/components/utils/Button';
 import SliderCarousel from '../src/components/utils/SliderCarousel';
 
-=======
->>>>>>> bfdfc66baf8019fd10f84118ca327db2cdd19c92
 export default function Home() {
   return (
     <div>
