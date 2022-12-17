8e4faa37ff437bd2840be6f81c8db22c1ee379ac
anteqkois <anteqkois.dev@gmail.com>
2022-12-17 03:03:58 +0100

    feat(api): add api endpoint to one auction

--- app/allAuction/page.tsx
+++ app/allAuction/page.tsx
@@ -8,11 +8,11 @@ export default function Page() {
     queryKey: ['auction'],
     queryFn: () => api('http://localhost:3000/api/auction'),
     suspense: true,
-    retry: false,
+    // retry: false,
     refetchInterval: 1000,
   });
 
-  console.log(data);
+  // console.log(data);
 
   return <main>{JSON.stringify(data?.data)}</main>;
 }
