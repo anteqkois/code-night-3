5dad6eb5a938e2f6dbdb098790f4a59fd3217a6d
eskeemos <l.michalik004@gmail.com>
2022-12-17 04:28:26 +0100

    q

--- app/auctions/page.tsx
+++ app/auctions/page.tsx
@@ -2,7 +2,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { api } from '@/lib/apiConfig';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 export default function Page() {
+  const router = useRouter();
   const { data, error } = useQuery({
     queryKey: ['auction'],
     queryFn: () => api('http://localhost:3000/api/auction'),
@@ -16,11 +18,11 @@ export default function Page() {
   
   return (
     <main className="bg-gradient-to-r from-primary-orange h-[calc(100vh-91px)]">
-      <h1 className="text-center pt-3">Samochody osobowe na licytacje</h1>
+      <div className='bg-gradient-to-l from-primary-orange my-5 py-5 text-center text-3xl'>Samochody osobowe na licytacje</div>
       <div className="grid grid-cols-4">
         {cars.map((car) => (
           <div className="my-4">
-          <div className="mx-auto w-72 h-60 text-center rounded-xl shadow-2xl bg-white">
+          <div className="mx-auto w-72 h-60 text-center rounded-xl shadow-2xl bg-white cursor-pointer" onClick={() => router.push(`/auction/${car.id}`)}>
             <h3 className="text-sm font-medium py-3 rounded-t-xl border-b-2 border-black">
               {car.mark} {car.model}
             </h3>
